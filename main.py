000ff4e51c4a872cb6f11e36a677e735b67145e7
Aleksandr Grekov <aleksgrekov94@gmail.com>
2023-03-30 21:44:53 +0500

    if added

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 def print_func(text):
     count = 0
 
-    while count <= 6:
+    while count < 6:
         print(text)
         count += 1
+    return count
 
 text = 'Hello! My name is Aleks'
 
-for _ in range(5):
-    print_func(text)
+if print_func(text) == 6:
+    print('This is not fun!')
