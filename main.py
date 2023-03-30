60b62f9609d7e2dded1e05ad6a6c66c5b1cdc8cd
Aleksandr Grekov <aleksgrekov94@gmail.com>
2023-03-30 21:07:43 +0500

    added function

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
-text = 'Hello! My name is Aleks'
-count = 0
+def print_func(text):
+    count = 0
 
-while count <= 6:
-    print(text)
-    count += 1
-    
+    while count <= 6:
+        print(text)
+        count += 1
+
+text = 'Hello! My name is Aleks'
+
 for _ in range(5):
-    print(text)
+    print_func(text)
 
 print ('<3 8===3')
 
 print(999)
+print (123)
